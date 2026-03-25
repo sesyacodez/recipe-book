1aaab4eff2e0b37770c1df0b21fddeb8556981bc
sesyacodez <stefania.koldovska@gmail.com>
2026-03-25 09:58:59 +0200

    fix auth check + login page

--- recipe_book/recipes/views.py
+++ recipe_book/recipes/views.py
@@ -1,31 +1,25 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 
 from .models import Recipe
 from .forms import RecipeForm
 
 
+@login_required
 def recipe_list(request):
     recipes = Recipe.objects.all()
-    return render(request, 'recipes/recipe_list.html', {'recipes': recipes})
+    return render(request, "recipes/recipe_list.html", {"recipes": recipes})
 
 
-from django.contrib.auth.decorators import login_required
-
-
-@login_required(login_url="/login/")
-def recipe_detail(request):
-    return render(request, "recipe_detail.html")
-
-
+@login_required
 def recipe_detail(request, recipe_id):
     recipe = get_object_or_404(Recipe, id=recipe_id)
-    return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
-
-from django.contrib.auth.decorators import login_required
+    return render(request, "recipes/recipe_detail.html", {"recipe": recipe})
 
 
+@login_required
 def recipe_create(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = RecipeForm(request.POST)
         if form.is_valid():
             recipe = form.save(commit=False)
@@ -34,27 +28,33 @@ def recipe_create(request):
                 recipe.user = request.user
             recipe.save()
             form.save_m2m()
-            return redirect('recipe_detail', recipe.id)
+            return redirect("recipe_detail", recipe.id)
     else:
         form = RecipeForm()
-    return render(request, 'recipes/recipe_form.html', {'form': form, 'create': True})
+    return render(request, "recipes/recipe_form.html", {"form": form, "create": True})
 
 
+@login_required
 def recipe_edit(request, recipe_id):
     recipe = get_object_or_404(Recipe, id=recipe_id)
-    if request.method == 'POST':
+    if request.method == "POST":
         form = RecipeForm(request.POST, instance=recipe)
         if form.is_valid():
             form.save()
-            return redirect('recipe_detail', recipe.id)
+            return redirect("recipe_detail", recipe.id)
     else:
         form = RecipeForm(instance=recipe)
-    return render(request, 'recipes/recipe_form.html', {'form': form, 'create': False, 'recipe': recipe})
+    return render(
+        request,
+        "recipes/recipe_form.html",
+        {"form": form, "create": False, "recipe": recipe},
+    )
 
 
+@login_required
 def recipe_delete(request, recipe_id):
     recipe = get_object_or_404(Recipe, id=recipe_id)
-    if request.method == 'POST':
+    if request.method == "POST":
         recipe.delete()
-        return redirect('recipe_list')
-    return render(request, 'recipes/recipe_confirm_delete.html', {'recipe': recipe})
+        return redirect("recipe_list")
+    return render(request, "recipes/recipe_confirm_delete.html", {"recipe": recipe})
